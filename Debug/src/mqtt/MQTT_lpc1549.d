4c5fe48469f1353b152f0e86f5a55e177a8a1e6f
jukkapekkaj <jukka-pekka.jaakkola@outlook.com>
2023-09-29 09:15:16 +0300

    Remove build files in Debug folder from version control

--- Debug/src/mqtt/MQTT_lpc1549.d
+++
@@ -1,7 +0,0 @@
-src/mqtt/MQTT_lpc1549.o src/mqtt/MQTT_lpc1549.d: \
- ../src/mqtt/MQTT_lpc1549.c ../src/mqtt/MQTT_lpc1549.h \
- C:\Users\jpj1\amk-opiskelu\3_vuosi\embedded_systems_programming\ventilation_controller\src/systick.h \
- ../src/mqtt/esp8266_socket.h
-../src/mqtt/MQTT_lpc1549.h:
-C:\Users\jpj1\amk-opiskelu\3_vuosi\embedded_systems_programming\ventilation_controller\src/systick.h:
-../src/mqtt/esp8266_socket.h:
--- Debug/src/mqtt/esp8266_socket.d
+++
@@ -1,9 +0,0 @@
-src/mqtt/esp8266_socket.o src/mqtt/esp8266_socket.d: \
- ../src/mqtt/esp8266_socket.c \
- C:\Users\jpj1\amk-opiskelu\3_vuosi\embedded_systems_programming\workspace\lpc_chip_15xx\inc/ring_buffer.h \
- C:\Users\jpj1\amk-opiskelu\3_vuosi\embedded_systems_programming\workspace\lpc_chip_15xx\inc/lpc_types.h \
- ../src/mqtt/esp8266_socket.h ../src/mqtt/serial_port.h
-C:\Users\jpj1\amk-opiskelu\3_vuosi\embedded_systems_programming\workspace\lpc_chip_15xx\inc/ring_buffer.h:
-C:\Users\jpj1\amk-opiskelu\3_vuosi\embedded_systems_programming\workspace\lpc_chip_15xx\inc/lpc_types.h:
-../src/mqtt/esp8266_socket.h:
-../src/mqtt/serial_port.h:
